9e0008b9d1522f50b26b20ee5a7c2e79ae1c6804
Andres De Castro <af.decastro879@gmail.com>
2015-10-14 15:33:45 -0500

    Cambio el numero de clinicas

--- SISARQ-Clinicas/src/com/sisarq/ThreadSender.java
+++ SISARQ-Clinicas/src/com/sisarq/ThreadSender.java
@@ -7,12 +7,13 @@ import java.util.Random;
  */
 public class ThreadSender implements Runnable {
     public static final String BASE_URL = "localhost:8080/sisarq/rest/receptor/";
+    private static final int NUM_CLINICAS = 80;
 
     @Override
     public void run() {
         Random rnd = new Random();
         try {
-            Main.sendGet(BASE_URL + rnd.nextInt(1000)+"/"+rnd.nextInt(500)+"/"+rnd.nextInt(400));
+            Main.sendGet(BASE_URL + rnd.nextInt(NUM_CLINICAS)+"/"+rnd.nextInt(500)+"/"+rnd.nextInt(400));
             Thread.sleep(1000);
         } catch (Exception e) {
             e.printStackTrace();
